882eb01626f00473feea66c1ebb0450cd2048a6c
Shankara21 <lazuardit21@gmail.com>
2022-08-16 19:48:58 +0700

    Updating ProductDetails Page

--- src/App.js
+++ src/App.js
@@ -5,18 +5,22 @@ import Details from "./pages/Details";
 import Cart from "./pages/Cart";
 import Congratulations from "./pages/Congratulations";
 import NotFound from "./pages/NotFound";
+import Provider from "./helpers/hooks/useGlobalContext";
 
 function App() {
   return (
-    <Router>
-      <Switch>
-        <Route exact path="/" component={HomePage} />
-        <Route path="/categories/:idc" component={Details} />
-        <Route path="/cart" component={Cart} />
-        <Route path="/congratulations" component={Congratulations} />
-        <Route path="*" component={NotFound} />
-      </Switch>
-    </Router>
+    <Provider>
+      <Router>
+        <Switch>
+          <Route exact path="/" component={HomePage} />
+          <Route exact path="/categories/:idc" component={Details} />
+          <Route path="/categories/:idc/products/:idp" component={Details} />
+          <Route path="/cart" component={Cart} />
+          <Route path="/congratulations" component={Congratulations} />
+          <Route path="*" component={NotFound} />
+        </Switch>
+      </Router>
+    </Provider>
   );
 }
 
--- src/pages/Details.js
+++ src/pages/Details.js
@@ -1,13 +1,106 @@
-import React from "react";
-import Clients from "../parts/Clients";
+import React, { useEffect } from "react";
 import Footer from "../parts/Footer";
 import Header from "../parts/Header";
 import Sitemap from "../parts/HomePage/Sitemap";
 import BreadCrumb from "../components/Breadcrumb";
 import ProductsDetails from "../parts/Details/ProductsDetails";
 import Suggestion from "../parts/Details/Suggestion";
+import useAsync from "../helpers/hooks/useAsync";
+import { useParams } from "react-router-dom";
+import fetchData from "../helpers/fetch";
+
+function LoadingSlider() {
+  return (
+    <section className="container mx-auto">
+      <div className="flex flex-wrap my-4 md:my-12">
+        <div className="w-full md:hidden px-4">
+          <div class="w-80 h-16 bg-gray-300 animate-pulse rounded-full"></div>
+          <div class="w-40 h-4 bg-gray-300 animate-pulse rounded-full"></div>
+        </div>
+        <div className="flex-1">
+          <div className="slider">
+            <div className="thumbnail">
+              {Array(5)
+                .fill()
+                .map((_, item) => {
+                  return (
+                    <div className="px-4 relative card group" key={item}>
+                      <div
+                        className="rounded-xl item bg-gray-300 animate-pulse"
+                        style={{ width: 106, height: 106 }}
+                      ></div>
+                    </div>
+                  );
+                })}
+            </div>
+            <div className="preview">
+              <div className="item rounded-lg h-full overflow-hidden">
+                <div class="item bg-gray-300 animate-pulse rounded-lg h-full overflow-hidden"></div>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div className="flex-1 px-4 md:p-6">
+          <div class="w-80 h-16 bg-gray-300 animate-pulse rounded-full"></div>
+          <div class="w-40 h-4 mt-4 bg-gray-300 animate-pulse rounded-full"></div>
+          <div class="w-44 h-10 mt-8 bg-gray-300 animate-pulse rounded-full"></div>
+
+          <hr className="my-8" />
+          <div className="w-36 h-4 mt-6 bg-gray-300 animate-pulse rounded-full"></div>
+          <div className="w-88 h-4 mt-6 bg-gray-300 animate-pulse rounded-full"></div>
+          <div className="w-40 h-4 mt-6 bg-gray-300 animate-pulse rounded-full"></div>
+          <div className="w-96 h-4 mt-6 bg-gray-300 animate-pulse rounded-full"></div>
+          <div className="w-64 h-4 mt-6 bg-gray-300 animate-pulse rounded-full"></div>
+          <div className="w-88 h-4 mt-6 bg-gray-300 animate-pulse rounded-full"></div>
+        </div>
+      </div>
+    </section>
+  );
+}
+
+function LoadingSuggestion() {
+  return (
+    <section className="bg-gray-100 px-4 py-16">
+      <div className="container mx-auto">
+        <div className="flex flex-start mb-4">
+          <h3 className="text-2xl capitalize font-semibold">
+            Complete your room <br className="" />
+            with what we designed
+          </h3>
+        </div>
+        <div className="flex overflow-x-auto mb-4 -mx-3">
+          {Array(4)
+            .fill()
+            .map((item) => {
+              return (
+                <div className="px-3 flex-none" style={{ width: 320 }}>
+                  <div className="rounded-xl p-4 pb-8 relative bg-white">
+                    <div className="rounded-xl overflow-hidden card-shadow w-full h-36">
+                      <div
+                        className="bg-gray-300 animate-pulse rounded-lg h-full overflow-hidden"
+                        style={{ width: 287, height: 150 }}
+                      ></div>
+                    </div>
+                    <div className="w-56 h-4 mt-6 bg-gray-300 animate-pulse rounded-full"></div>
+                    <div className="w-40 h-4 mt-3 bg-gray-300 animate-pulse rounded-full"></div>
+                  </div>
+                </div>
+              );
+            })}
+        </div>
+      </div>
+    </section>
+  );
+}
 
 export default function Details() {
+  const { idp } = useParams();
+
+  const { data, error, run, isLoading, isError } = useAsync();
+
+  React.useEffect(() => {
+    run(fetchData({ url: `/api/products/${idp}` }));
+  }, [run]);
   return (
     <>
       <Header theme="black" />
@@ -18,8 +111,13 @@ export default function Details() {
           { url: "/categories/912313/products/91283", name: "Details" },
         ]}
       />
-      <ProductsDetails />
-      <Suggestion />
+      {isLoading ? <LoadingSlider /> : <ProductsDetails data={data} />}
+      {isLoading ? (
+        <LoadingSuggestion />
+      ) : (
+        <Suggestion data={data?.relatedProducts || {}} />
+      )}
+
       {/* <Clients /> */}
       <Sitemap />
       <Footer />
